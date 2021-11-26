4255357963d59118d1ae1256b7d7afba050a06b4
Benoit Daloze <eregontp@gmail.com>
2021-11-26 19:44:12 +0100

    plot

---
+++ plot_all.rb
@@ -0,0 +1,20 @@
+macros = Dir["benchmarks/*/benchmark.rb"].sort
+micros = Dir["benchmarks/*.rb"].sort
+benchmarks = macros + micros + ['benchmarks/geomean.rb']
+
+results_dir = ARGV.fetch(0)
+
+benchmarks.each do |benchmark|
+  benchmark_name = if benchmark.end_with?('/benchmark.rb')
+    File.basename(File.dirname(benchmark))
+  else
+    File.basename(benchmark, '.rb')
+  end
+
+  next if benchmark_name == 'jekyll'
+  puts benchmark_name
+
+  files = Dir["#{results_dir}/#{benchmark_name}/results-*"]
+  out_file = "graphs/#{benchmark_name}"
+  system 'python3', 'plot_benchmark.py', '--out_file', out_file, *files, exception: true
+end
